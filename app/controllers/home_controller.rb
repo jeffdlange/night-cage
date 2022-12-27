62ae4170d98b79cadea35681feb56f7196ee9be1
Jeff Lange <jeffersonian.lange@gmail.com>
2022-12-27 13:00:07 -0500

    Related view/controller updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    @game_started = params[:starting_tiles].present?
+    @game = game_params.present? ? Game.new(game_params) : BlankGame.new
+  end
+
+  private
+
+  def game_params
+    params[:game]
   end
 end
